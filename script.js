37a3d409d7f21000366ccb78287dbc827f55c265
Tapok <mgilkov@gmail.com>
2022-02-17 16:42:12 +1000

    Добавил скрипты

---
+++ script.js
@@ -0,0 +1,23 @@
+function print_contact(mySkype, myTelegram) {
+
+    const skype = mySkype;
+    const Telegram = myTelegram;
+
+    alert('skype: ' + skype +'\nTel: ' + Telegram + '\nTelegram: @helper-store');
+
+    console.log('Пользователь обратился в тех.поддержку');
+}
+
+function sayHello(name) {
+    alert('Привет, ' + name);
+}
+
+
+function namepers(nameperson) {
+
+    const skidkaname = 'Максим';
+
+    alert('Специально для вас' + skidkaname +  'персональная скидка 20%');
+
+    console.log(skidkaname);
+}
